d5668885ec31ffcf06bcfa93881ff3027ced25a3
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-09-13 18:29:00 +0500

    Ready

--- storage/postgres/postgres.go
+++ storage/postgres/postgres.go
@@ -75,102 +75,71 @@ func ListUsers() ([]models.User, error) {
 
 
 func UpdateUser(id, username, email string) (bool, error) {
-	tx, err := DB.Begin()
+	stmt1, err := DB.Prepare("UPDATE users SET username=$1, email=$2 WHERE id=$3")
 	if err != nil {
-		return false, fmt.Errorf("transaction begin error: %v", err)
-	}
-
-	stmt1, err := tx.Prepare("UPDATE users SET username=$1, email=$2 WHERE id=$3")
-	if err != nil {
-		tx.Rollback()
 		return false, fmt.Errorf("prepare statement error for server 2: %v", err)
 	}
 	defer stmt1.Close()
 
 	res1, err := stmt1.Exec(username, email, id)
 	if err != nil {
-		tx.Rollback()
 		return false, fmt.Errorf("execute statement error for server 2: %v", err)
 	}
 
-	stmt2, err := tx.Prepare("UPDATE users_server2 SET username=$1, email=$2 WHERE id=$3")
+	stmt2, err := DB.Prepare("UPDATE users_server2 SET username=$1, email=$2 WHERE id=$3")
 	if err != nil {
-		tx.Rollback()
 		return false, fmt.Errorf("prepare statement error for server 1: %v", err)
 	}
 	defer stmt2.Close()
 
 	res2, err := stmt2.Exec(username, email, id)
 	if err != nil {
-		tx.Rollback()
 		return false, fmt.Errorf("execute statement error for server 1: %v", err)
 	}
 
 	rowsAffected1, err := res1.RowsAffected()
 	if err != nil || rowsAffected1 == 0 {
-		tx.Rollback()
 		return false, fmt.Errorf("no rows affected for server 2")
 	}
 
 	rowsAffected2, err := res2.RowsAffected()
 	if err != nil || rowsAffected2 == 0 {
-		tx.Rollback()
 		return false, fmt.Errorf("no rows affected for server 1")
 	}
-
-	if err := tx.Commit(); err != nil {
-		return false, fmt.Errorf("transaction commit error: %v", err)
-	}
-
 	return true, nil
 }
 
 func DeleteUser(id string) (bool, error) {
-	tx, err := DB.Begin()
+	stmt1, err := DB.Prepare("DELETE FROM users WHERE id=$1")
 	if err != nil {
-		return false, fmt.Errorf("transaction begin error: %v", err)
-	}
-
-	stmt1, err := tx.Prepare("DELETE FROM users WHERE id=$1")
-	if err != nil {
-		tx.Rollback()
 		return false, fmt.Errorf("prepare statement error for server 1: %v", err)
 	}
 	defer stmt1.Close()
 
 	res1, err := stmt1.Exec(id)
 	if err != nil {
-		tx.Rollback()
 		return false, fmt.Errorf("execute statement error for server 1: %v", err)
 	}
 
-	stmt2, err := tx.Prepare("DELETE FROM users_server2 WHERE id=$1")
+	stmt2, err := DB.Prepare("DELETE FROM users_server2 WHERE id=$1")
 	if err != nil {
-		tx.Rollback()
 		return false, fmt.Errorf("prepare statement error for server 1: %v", err)
 	}
 	defer stmt2.Close()
 
 	res2, err := stmt2.Exec(id)
 	if err != nil {
-		tx.Rollback()
 		return false, fmt.Errorf("execute statement error for server 1: %v", err)
 	}
 
 	rowsAffected1, err := res1.RowsAffected()
 	if err != nil || rowsAffected1 == 0 {
-		tx.Rollback()
 		return false, fmt.Errorf("no rows affected for server 2")
 	}
 
 	rowsAffected2, err := res2.RowsAffected()
 	if err != nil || rowsAffected2 == 0 {
-		tx.Rollback()
 		return false, fmt.Errorf("no rows affected for server 1")
-	}
-
-	if err := tx.Commit(); err != nil {
-		return false, fmt.Errorf("transaction commit error: %v", err)
 	}
 
 	return true, nil
